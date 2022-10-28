69239ca61e91d9b062eca1874e938f09c5c2fc83
Pocket Tiki <pockettikibar@gmail.com>
2022-10-27 20:46:21 -0400

    added missing ingredients table

--- app.R
+++ app.R
@@ -11,21 +11,25 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(DT)
+library(shinyWidgets)
+library(shinydashboard)
 
 #xxx...from category specific checkbox groups, combine all checked to filter
 
 data <- read_csv(here('pocket_tiki_data.csv')) %>% 
   rename(ingredients_string = ingredients_list) %>% 
-  mutate(ingredients_list = as.list(strsplit(ingredients_string, ", ")))
+  mutate(ingredients_list = as.list(strsplit(ingredients_string, ", "))) %>% 
+  mutate(num_ing = lengths(ingredients_list))
 
 ing_categories <- read_csv(here("ingredient_categories.csv"))
 
 all_ingredients <- as_tibble(unique(unlist(strsplit(data$ingredients_string, ", ")))) %>% 
   rename(ingredient = value) %>% 
   left_join(ing_categories, by = 'ingredient')
+# Tue Oct 25 15:16:21 2022 ------------------------------
 
 fruit <- all_ingredients %>% filter(category == "fruit")
-misc <- all_ingredients %>% filter(category == "misc")
+misc <- all_ingredients %>% filter(category == "misc" | is.na(category))
 juice <- all_ingredients %>% filter(category == "juice")
 syrup <- all_ingredients %>% filter(category == "syrup")
 liqueur <- all_ingredients %>% filter(category == "liqueur")
@@ -35,92 +39,104 @@ mixer <- all_ingredients %>% filter(category == "mixer")
 spice <- all_ingredients %>% filter(category == "spice")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    #titlePanel(""),
-
-    # Sidebar with a slider input for number of bins 
-    fluidRow(
-      column(2,
-             checkboxGroupInput(inputId = "spirit_checkbox", label = "spirits",
-                                choices = spirit$ingredient,
-                                selected = spirit$ingredient[spirit$initially_selected == "yes"]),
-             checkboxGroupInput(inputId = "spice_checkbox", label = "spices",
-                                choices = spice$ingredient,
-                                selected = spice$ingredient[spice$initially_selected == "yes"])
-             ),
-      column(2,
-             checkboxGroupInput(inputId = "liqueur_checkbox", label = "liqueurs",
-                                choices = liqueur$ingredient,
-                                selected = liqueur$ingredient[liqueur$initially_selected == "yes"]),
-             checkboxGroupInput(inputId = "bitters_checkbox", label = "bitters",
-                                choices = bitters$ingredient,
-                                selected = bitters$ingredient[bitters$initially_selected == "yes"]),
-             checkboxGroupInput(inputId = "misc_checkbox", label = "misc",
-                                choices = misc$ingredient,
-                                selected = misc$ingredient[misc$initially_selected == "yes"])
-             ),
-      column(2,
-             checkboxGroupInput(inputId = "juice_checkbox", label = "jucies",
-                               choices = juice$ingredient,
-                               selected = juice$ingredient[juice$initially_selected == "yes"]),
-             checkboxGroupInput(inputId = "fruit_checkbox", label = "fruits",
-                                choices = fruit$ingredient,
-                                selected = fruit$ingredient[fruit$initially_selected == "yes"]),
-             checkboxGroupInput(inputId = "syrup_checkbox", label = "syrups",
-                                choices = syrup$ingredient,
-                                selected = syrup$ingredient[syrup$initially_selected == "yes"]),
-             
-             checkboxGroupInput(inputId = "mixer_checkbox", label = "mixers",
-                                choices = mixer$ingredient,
-                                selected = mixer$ingredient[mixer$initially_selected == "yes"])
-             ),
-      column(6,
-             #textOutput(outputId = "available_ingredients_text"),
-             DT::dataTableOutput(outputId = "drinks_you_can_make_table")
-             # full, unfiltered table
-             # DT::datatable(data,
-             #               options = list(pageLength = 10),
-             #               rownames = FALSE,
-             #               colnames = c("Cocktail Name", "Ingredients List", "Recipe", "Glass Type", "Source"),
-             #               filter = 'top')
-             )
-    ) # end fluid row
-    ) # end fluid page
-     
-         
-          # checkboxGroupInput(inputId = "ingredients_you_have", label = "select ingredients that you have",
-          #                    choices = all_ingredients,
-          #                    # selected = all_ingredients
-          #                    # selected = c("rum", "lime juice", "Demerara syrup")
-          #                    selected = all_ingredients[1:160])
-    
-
-        # Show a plot of the generated distribution
-   
-          # p("missing ingredients"),
-          # textOutput(outputId = "mis_ing"),
-          # # put table here
-          # DT::dataTableOutput(outputId = "drinks_you_can_make_table")
-           
+ui <- dashboardPage(
+  dashboardHeader(title = "Pocket Tiki"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(h3("Spirits"),
+               pickerInput(
+                 inputId = "spirit_picker",
+                 choices = spirit$ingredient,
+                 selected = spirit$ingredient[spirit$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Liqueurs"),
+               pickerInput(
+                 inputId = "liqueur_picker",
+                 choices = liqueur$ingredient,
+                 selected = liqueur$ingredient[liqueur$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Juices"),
+               pickerInput(
+                 inputId = "juice_picker",
+                 choices = juice$ingredient,
+                 selected = juice$ingredient[juice$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Fruit"),
+               pickerInput(
+                 inputId = "fruit_picker",
+                 choices = fruit$ingredient,
+                 selected = fruit$ingredient[fruit$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Syrup"),
+               pickerInput(
+                 inputId = "syrup_picker", label = h4("Syrups"),
+                 choices = syrup$ingredient,
+                 selected = syrup$ingredient[syrup$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Mixers"),
+               pickerInput(
+                 inputId = "mixer_picker",
+                 choices = mixer$ingredient,
+                 selected = mixer$ingredient[mixer$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Bitters"),
+               pickerInput(
+                 inputId = "bitters_picker", label = h4("Bitters"),
+                 choices = bitters$ingredient,
+                 selected = bitters$ingredient[bitters$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Spices"),
+               pickerInput(
+                 inputId = "spice_picker", label = h4("Spices"),
+                 choices = spice$ingredient,
+                 selected = spice$ingredient[spice$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE)),
+      menuItem(h3("Miscellaneous"),
+               pickerInput(
+                 inputId = "misc_picker", label = h4("Miscellaneous"),
+                 choices = misc$ingredient,
+                 selected = misc$ingredient[misc$initially_selected == "yes"],
+                 options = list(`actions-box` = TRUE),
+                 multiple = TRUE))
+    ) # end sidebar menu
+  ), # end dashboard sidebar
+  dashboardBody(
+    #DT::dataTableOutput(outputId = "drinks_you_can_make_table"),
+    tabsetPanel(
+      tabPanel(title = "Drinks Made With Only Selected Ingredients",
+               DT::dataTableOutput(outputId = "drinks_you_can_make_table")),
+      tabPanel(title = "All Drinks",
+               textOutput(outputId = "mis_ing"),
+               DT::dataTableOutput(outputId = "missing_ingredient_table"))
+    )
+  ) # end dashboard body
+) # end dashboard page
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  missing_ingredients <- reactive({
-    setdiff(all_ingredients, input$ingredients_you_have)
-  })
-  
-  output$mis_ing <- renderPrint({
-    missing_ingredients()
-  })
-  
   available_ingredients <- reactive({
-    as.list(c(input$spirit_checkbox, input$spice_checkbox, input$liqueur_checkbox, input$bitters_checkbox, input$misc_checkbox, input$juice_checkbox, input$fruit_checkbox, input$syrup_checkbox, input$mixer_checkbox))
+    as.list(c(input$spirit_picker, input$spice_picker, input$liqueur_picker, input$bitters_picker, input$misc_picker, input$juice_picker, input$fruit_picker, input$syrup_picker, input$mixer_picker))
   })
   
-  output$available_ingredients_text <- renderText(as.character(available_ingredients()))
+  missing_ingredients <- reactive({
+    setdiff(all_ingredients$ingredient, available_ingredients())
+  })
+  
+  output$mis_ing <- renderText({
+    as.character(missing_ingredients())
+  })
+  
+  #output$available_ingredients_text <- renderText(as.character(available_ingredients()))
   
   # function to determine if there is at least 1 ingredient in the recipe that is not available
   can_you_make_drink <- reactive({
@@ -136,49 +152,99 @@ server <- function(input, output) {
       }
     }
   })
-    
+  
   results <- reactive({
     apply(data, 1, can_you_make_drink())
   })
-    
+  
   data_for_table <- reactive({
     data %>% 
       cbind(result = results()) %>% 
-      filter(result == "can make")
+      filter(result == "can make") %>% 
+      select(1:5)
   })
-    
   
-  # drinks_you_can_make <- reactive({
-  #     data %>%
-  #     filter(!grepl(paste(missing_ingredients(), collapse = "|"), ingredients_list))
-  #     })
-  
-  rum_drinks <- data %>% 
-    filter(grepl("rum", ingredients_list))
-  
-  drinks_you_can_make <- reactive({
-    data %>% 
-      filter(all(ingredients_list %in% available_ingredients()))
-  })
-
   output$drinks_you_can_make_table <- DT::renderDataTable({
     DT::datatable(data_for_table(),
-                  options = list(pageLength = 10),
+                  options = list(pageLength = 25),
                   rownames = FALSE,
                   colnames = c("Cocktail Name", "Ingredients List", "Recipe", "Glass Type", "Source"),
                   filter = 'top')
   })
   
+  # number_available_ingredients <- reactive({
+  #   function(df){
+  #     ing_unlist_DT2 <- unlist(df$ingredients_list)
+  #     ing_list_DT2 <- as.list(ing_unlist_DT2)
+  #     num_ing_avail <- sum(ing_list_DT2 %in% available_ingredients())
+  #     return(num_ing_avail)
+  #   }
+  # })
+  # 
+  # result_DT2 <- reactive({
+  #   apply(data, 1, number_available_ingredients())
+  # })
   
-  output$data_table <- DT::renderDataTable({
-    DT::datatable(data,
-                  options = list(pageLength = 10),
-                  rownames = FALSE,
-                  colnames = c("Cocktail Name", "Ingredients List", "Recipe", "Glass Type", "Source"),
-                  filter = 'top')
+  # data_for_table2 <- reactive({
+  #   data %>% 
+  #     cbind(num_ingredients_available = result_DT2()) %>% 
+  #     filter(num_ingredients_available > 0)
+  #     #select(1:5)
+  # })
+  # 
+  # output$drinks_with_at_least_1_ingredient_table <- DT::renderDataTable({
+  #   DT::datatable(data_for_table2(),
+  #                 options = list(pageLength = 25),
+  #                 rownames = FALSE,
+  #                 #colnames = c("Cocktail Name", "Ingredients List", "Recipe", "Glass Type", "Source"),
+  #                 filter = 'top')
+  # })
+  
+  number_missing_ingredients_fun <- reactive({ function(df){
+    ing_unlist_1 <- unlist(df$ingredients_list)
+    ing_list_1 <- as.list(ing_unlist_1)
+    num_missing <- sum(!(ing_list_1 %in% available_ingredients()))
+    return(num_missing)
+  }
+  })
+  
+  num_missing_result <- reactive({
+    apply(data, 1, number_missing_ingredients_fun())
+  })
+  
+  missing_ingredients_fun <- reactive({
+    function(df){
+      ing_unlist_2 <- unlist(df$ingredients_list)
+      ing_list_2 <- as.list(ing_unlist_2)
+      missing_ing <- toString(setdiff(ing_list_2, available_ingredients()))
+      return(missing_ing)
+    }
+  })
+  
+  missing_ing_result <- reactive({
+    apply(data, 1, missing_ingredients_fun())
+  })
+  
+  data_for_table3 <- reactive({
+    data %>% 
+      cbind(num_ingredients_missing = num_missing_result()) %>% 
+      mutate(x_of_y = paste(num_ingredients_missing, "of", num_ing)) %>% 
+      cbind(missing_ingredients_list = missing_ing_result()) %>% 
+      select(1:5, 9, 10) %>% 
+      select(1, 2, 6, 7, 3, 4, 5)
   })
 
-}
+  
+  output$missing_ingredient_table <- DT::renderDataTable({
+    DT::datatable(data_for_table3(),
+                  options = list(pageLength = 25),
+                  rownames = FALSE,
+                  colnames = c("Cocktail Name", "Ingredients List", "Number of Available Ingredients", "Missing Ingredients", "Recipe", "Glass Type", "Source"),
+                  filter = 'top')
+  })
+  
+
+} # end server function
 
 # Run the application 
 shinyApp(ui = ui, server = server)
