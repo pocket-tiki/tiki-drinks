b690ccca8041258660fb45e1bfa641f05b618807
pocket-tiki <pockettikibar@gmail.com>
2023-02-11 13:55:43 -0700

    fixed table name

--- app.R
+++ app.R
@@ -17,7 +17,6 @@ library(sass)
 library(gt)
 library(gtExtras)
 
-
 smugglers_cove_data <- read_csv(here('smugglers_cove.csv')) %>% 
   rename(ingredients_string = ingredients_list) %>% 
   mutate(ingredients_list = as.list(strsplit(ingredients_string, ", "))) %>% 
@@ -95,406 +94,76 @@ ui <- dashboardPage(
     width = 300,
     sidebarMenu(
       menuItem(h3("Spirits"),
-               # radioGroupButtons(
-               #   inputId = "spiritRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   # Add a class to the buttons, you can use Bootstrap status like 'info', 'primary', 'danger' (red), 'warning' or 'success' (green). Or use an arbitrary strings to add a custom class, e.g. : with status = "custom-class", buttons will have class btn-custom-class.
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info",
-               #   size = "normal" # “xs”, “sm”, “normal”, “lg”
-               # ),
-               # tags$script("$(\"input:radio[name='spiritRadioButtons'][value='Default']\").parent().css('background-color', '#DE6B63');"),
-               # tags$script("$(\"input:radio[name='spiritRadioButtons'][value='Select All']\").parent().css('background-color', '#EDB6B2');"),
-               # tags$script("$(\"input:radio[name='spiritRadioButtons'][value='Select None']\").parent().css('background-color', '#E7E7E7');"),
                awesomeCheckboxGroup(
                  inputId = "spirit_checkbox", label = "", 
                  choices = spirit_choices,
                  selected = spirit_initial
                )),
       menuItem(h3("Liqueurs"),
-               # radioGroupButtons(
-               #   inputId = "liqueurRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   # Add a class to the buttons, you can use Bootstrap status like 'info', 'primary', 'danger' (red), 'warning' or 'success' (green). Or use an arbitrary strings to add a custom class, e.g. : with status = "custom-class", buttons will have class btn-custom-class.
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info",
-               #   size = "normal"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "liqueur_checkbox", label = "", 
                  choices = liqueur_choices,
                  selected = liqueur_initial
                )),
       menuItem(h3("Juice"),
-               # radioGroupButtons(
-               #   inputId = "juiceRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "juice_checkbox", label = "", 
                  choices = juice_choices,
                  selected = juice_initial
                )),
       menuItem(h3("Fruit"),
-               # radioGroupButtons(
-               #   inputId = "fruitRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "fruit_checkbox", label = "", 
                  choices = fruit_choices,
                  selected = fruit_initial
                )),
       menuItem(h3("Syrup"),
-               # radioGroupButtons(
-               #   inputId = "syrupRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "syrup_checkbox", label = "", 
                  choices = syrup_choices,
                  selected = syrup_initial
                )),
       menuItem(h3("Mixers"),
-               # radioGroupButtons(
-               #   inputId = "mixerRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "mixer_checkbox", label = "", 
                  choices = mixer_choices,
                  selected = mixer_initial
                )),
       menuItem(h3("Bitters"),
-               # radioGroupButtons(
-               #   inputId = "bittersRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "bitters_checkbox", label = "", 
                  choices = bitters_choices,
                  selected = bitters_initial
                )),
       menuItem(h3("Spices"),
-               # radioGroupButtons(
-               #   inputId = "spiceRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "spice_checkbox", label = "", 
                  choices = spice_choices,
                  selected = spice_initial
                )),
       menuItem(h3("Miscellaneous"),
-               # radioGroupButtons(
-               #   inputId = "miscRadioButtons",
-               #   label = "", 
-               #   choices = c("Default", "Select All", "Select None"),
-               #   selected = "Default",
-               #   individual = TRUE,
-               #   checkIcon = list(
-               #     yes = tags$i(class = "fa fa-circle", 
-               #                  style = "color: #1ea896"),
-               #     no = tags$i(class = "fa fa-circle-o", 
-               #                 style = "color: #1ea896")),
-               #   #status = "info"
-               # ),
                awesomeCheckboxGroup(
                  inputId = "misc_checkbox", label = "", 
                  choices = misc_choices,
                  selected = misc_initial
                ))
-      # menuItem(h3("xMiscellaneous"),
-      #          pickerInput(
-      #            inputId = "misc_picker", label = h4("Miscellaneous"),
-      #            choices = misc_choices,
-      #            selected = misc_initial,
-      #            options = list(`actions-box` = TRUE),
-      #            multiple = TRUE)),
-     
-      # menuItem(h3("test...select all")
-               # sliderTextInput(
-               #   inputId = "mySliderText", 
-               #   label = "Your choice:", 
-               #   grid = TRUE, 
-               #   force_edges = FALSE,
-               #   choices = c("Select Name", "Default Selection", "Select All"),
-               #   selected = "Default Selection")
-               # awesomeCheckboxGroup(
-               #   inputId = "mtcars",
-               #   label = "mtcars column variables", 
-               #   choices = myChoices,
-               #   selected = initialChoices
-               # )
-               # )
     ) # end sidebar menu
   ), # end dashboard sidebar
   dashboardBody(
-    # tags$head(
-    #   tags$link(
-    #     rel = "stylesheet",
-    #     type = "text/css",
-    #     #href = "shinydashboard_default.css"
-    #     href = "tiki.css")
-    # ),
     tags$head(
       tags$style(sass(sass_file("styles/tiki_sass.scss")))
     ),
-    # tags$head(tags$style(HTML('
-    #     .irs-grid-text {
-    #     position: absolute;
-    #     bottom: 0;
-    #     left: 0;
-    #     white-space: nowrap;
-    #     text-align: center;
-    #     font-size: 19px;
-    #     line-height: 9px;
-    #     padding: 0 3px;}
-    #                           '))),
-    # tags$head(
-    #   tags$link(rel = "stylesheet", type = "text/css", href = "shinydashboard_default.css")),
     tabsetPanel(
       tabPanel(title = h4("Drinks Made With Only Selected Ingredients"),
                DT::dataTableOutput(outputId = "drinks_you_can_make_table")),
       tabPanel(title = h4("All Drinks"),
-               #textOutput(outputId = "mis_ing"),
                DT::dataTableOutput(outputId = "missing_ingredient_table"))
-      # tabPanel(title = h4("test"),
-      #          DT::dataTableOutput(outputId = "test_table"))
     )
   ) # end dashboard body
 ) # end dashboard page
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, 'mtcars', choices = myChoices,
-  #     selected = test_selected()
-  #   )
-  # })
-   
-  # spirit_selected <- reactive({
-  #   if (input$spiritRadioButtons == "Default") {
-  #     return(spirit_initial)
-  #   } else if (input$spiritRadioButtons == "Select All") {
-  #     return(spirit_choices)
-  #   } else if (input$spiritRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'spirit_checkbox',
-  #     selected = spirit_selected()
-  #   )
-  # })
-  # 
-  # liqueur_selected <- reactive({
-  #   if (input$liqueurRadioButtons == "Default") {
-  #     return(liqueur_initial)
-  #   } else if (input$liqueurRadioButtons == "Select All") {
-  #     return(liqueur_choices)
-  #   } else if (input$liqueurRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'liqueur_checkbox',
-  #     selected = liqueur_selected()
-  #   )
-  # })
-  # 
-  # juice_selected <- reactive({
-  #   if (input$juiceRadioButtons == "Default") {
-  #     return(juice_initial)
-  #   } else if (input$juiceRadioButtons == "Select All") {
-  #     return(juice_choices)
-  #   } else if (input$juiceRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'juice_checkbox',
-  #     selected = juice_selected()
-  #   )
-  # })
-  
-  # fruit_selected <- reactive({
-  #   if (input$fruitRadioButtons == "Default") {
-  #     return(fruit_initial)
-  #   } else if (input$fruitRadioButtons == "Select All") {
-  #     return(fruit_choices)
-  #   } else if (input$fruitRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'fruit_checkbox',
-  #     selected = fruit_selected()
-  #   )
-  # })
-  # 
-  # syrup_selected <- reactive({
-  #   if (input$syrupRadioButtons == "Default") {
-  #     return(syrup_initial)
-  #   } else if (input$syrupRadioButtons == "Select All") {
-  #     return(syrup_choices)
-  #   } else if (input$syrupRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'syrup_checkbox',
-  #     selected = syrup_selected()
-  #   )
-  # })
-  # 
-  # mixer_selected <- reactive({
-  #   if (input$mixerRadioButtons == "Default") {
-  #     return(mixer_initial)
-  #   } else if (input$mixerRadioButtons == "Select All") {
-  #     return(mixer_choices)
-  #   } else if (input$mixerRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'mixer_checkbox',
-  #     selected = mixer_selected()
-  #   )
-  # })
-  # 
-  # bitters_selected <- reactive({
-  #   if (input$bittersRadioButtons == "Default") {
-  #     return(bitters_initial)
-  #   } else if (input$bittersRadioButtons == "Select All") {
-  #     return(bitters_choices)
-  #   } else if (input$bittersRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'bitters_checkbox',
-  #     selected = bitters_selected()
-  #   )
-  # })
-  # 
-  # spice_selected <- reactive({
-  #   if (input$spiceRadioButtons == "Default") {
-  #     return(spice_initial)
-  #   } else if (input$spiceRadioButtons == "Select All") {
-  #     return(spice_choices)
-  #   } else if (input$spiceRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'spice_checkbox',
-  #     selected = spice_selected()
-  #   )
-  # })
-  
-  # misc_selected <- reactive({
-  #   if (input$miscRadioButtons == "Default") {
-  #     return(misc_initial)
-  #   } else if (input$miscRadioButtons == "Select All") {
-  #     return(misc_choices)
-  #   } else if (input$miscRadioButtons == "Select None") {
-  #     return(NA)
-  #   }
-  # })
-  # 
-  # observe({
-  #   updateAwesomeCheckboxGroup(
-  #     session, inputId = 'misc_checkbox',
-  #     selected = misc_selected()
-  #   )
-  # })
   
   available_ingredients <- reactive({
     as.list(c(input$spirit_checkbox, input$liqueur_checkbox, input$juice_checkbox, input$fruit_checkbox, input$syrup_checkbox, input$mixer_checkbox, input$bitters_checkbox, input$spice_checkbox, input$misc_checkbox))
@@ -507,8 +176,6 @@ server <- function(input, output, session) {
   output$mis_ing <- renderText({
     as.character(missing_ingredients())
   })
-  
-  #output$available_ingredients_text <- renderText(as.character(available_ingredients()))
   
   # function to determine if there is at least 1 ingredient in the recipe that is not available
   can_you_make_drink <- reactive({
@@ -538,7 +205,7 @@ server <- function(input, output, session) {
   
   output$drinks_you_can_make_table <- DT::renderDataTable({
     DT::datatable(data_for_table(),
-                  options = list(pageLength = 25),
+                  options = list(pageLength = 50),
                   rownames = FALSE,
                   colnames = c("Cocktail Name", "Ingredients List", "Recipe", "Glass Type", "Source"),
                   filter = 'top',
@@ -573,7 +240,7 @@ server <- function(input, output, session) {
   data_for_missing_table <- reactive({
     data %>% 
       cbind(num_ingredients_missing = num_missing_result()) %>% 
-      mutate(x_of_y = paste(num_ingredients_missing, "of", num_ing)) %>% 
+      mutate(x_of_y = paste(num_ingredients_missing, "of", num_ing)) %>%
       cbind(missing_ingredients_list = missing_ing_result()) %>% 
       mutate(missing_ingredients_list = str_replace_all(missing_ingredients_list, ", ", ",<br>")) %>% 
       select(1, 8, 10, 11, 3:5)
@@ -584,27 +251,10 @@ server <- function(input, output, session) {
     DT::datatable(data_for_missing_table(),
                   options = list(pageLength = 25),
                   rownames = FALSE,
-                  colnames = c("Cocktail Name", "Ingredients List", "Number of Available Ingredients", "Missing Ingredients", "Recipe", "Glass Type", "Source"),
+                  colnames = c("Cocktail Name", "Ingredients List", "Number of Missing Ingredients", "Missing Ingredients", "Recipe", "Glass Type", "Source"),
                   filter = 'top',
                   escape = FALSE)
   })
-  
-  # data_for_test_table <- reactive({
-  #   data %>% 
-  #     cbind(result = results()) %>% 
-  #     filter(result == "can make") %>% 
-  #     select(1:5) %>% 
-  #     mutate(ingredients_linebreak = str_replace_all(ingredients_string, ", ", "<br>"))
-  # })
-  # 
-  # output$test_table <- DT::renderDataTable({
-  #   DT::datatable(data_for_test_table(),
-  #                 options = list(pageLength = 25),
-  #                 rownames = FALSE,
-  #                 filter = 'top',
-  #                 escape = FALSE
-  #   )
-  # })
   
 
 } # end server function
